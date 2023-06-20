369c24738a8eb385cfcaee3c6c17c56be7550097
NateRiz <nathan.rizik@gmail.com>
2023-06-19 21:09:02 -0700

    setup +

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='ReprTraceback',
     version='1.0.0',
     description='Improve stack traces with the values associated with each argument in each frame.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='NateRiz',
     author_email='natezriz@gmail.com',
     url='https://github.com/NateRiz/ReprTraceback',
